df4e9fd6235cbf4aad440eb04273921450d19067
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-05-19 15:10:38 -0700

    add a bit better string encoding support, shuffle code around

--- ransom/url.py
+++ ransom/url.py
@@ -4,10 +4,32 @@ from compat import (unicode, bytes, OrderedDict, StringIO,
                     urlparse, urlunparse, urlencode, quote)
 
 
-# The unreserved URI characters (RFC 3986)
-UNRESERVED_CHARS = frozenset(
+"""
+TODO:
+ - url param support
+ - support ';' in addition to '&' for url params
+   - http://www.w3.org/TR/REC-html40/appendix/notes.html#h-B.2.2
+"""
+
+DEFAULT_ENCODING = 'utf-8'
+
+
+# The unreserved URI characters (per RFC 3986)
+_UNRESERVED_CHARS = frozenset(
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     "0123456789-._~")
+
+
+# IRI escaped character ranges (per RFC3987)
+_ESC_RANGES = [(0xA0, 0xD7FF),
+               (0xE000, 0xF8FF),
+               (0xF900, 0xFDCF),
+               (0xFDF0, 0xFFEF),
+               (0xE1000, 0xFFFFD),
+               (0xF0000, 0xFFFFD),
+               (0x100000, 0x10FFFD)]
+_ESC_RANGES.extend([(i, i + 0xFFFD) for i in range(0x10000, 0xE0000, 0x10000)])
+_ESC_RANGES.sort(key=lambda x: x[0])
 
 
 def unquote_unreserved(url):
@@ -20,7 +42,7 @@ def unquote_unreserved(url):
         h = parts[i][0:2]
         if len(h) == 2 and h.isalnum():
             c = chr(int(h, 16))
-            if c in UNRESERVED_CHARS:
+            if c in _UNRESERVED_CHARS:
                 parts[i] = c + parts[i][2:]
             else:
                 parts[i] = '%' + parts[i]
@@ -33,19 +55,7 @@ def requote(url):
     return quote(unquote_unreserved(url), safe="!#$%&'()*+,/:;=?@[]~")
 
 
-# per RFC3987
-_ESC_RANGES = [(0xA0, 0xD7FF),
-               (0xE000, 0xF8FF),
-               (0xF900, 0xFDCF),
-               (0xFDF0, 0xFFEF),
-               (0xE1000, 0xFFFFD),
-               (0xF0000, 0xFFFFD),
-               (0x100000, 0x10FFFD)]
-_ESC_RANGES.extend([(i, i + 0xFFFD) for i in range(0x10000, 0xE0000, 0x10000)])
-_ESC_RANGES.sort(key=lambda x: x[0])
-
-
-def parse_url(url_str, encoding='utf-8'):
+def parse_url(url_str, encoding=DEFAULT_ENCODING):
     if not isinstance(url_str, unicode):
         try:
             url_str = url_str.decode(encoding)
@@ -60,8 +70,10 @@ class URL(object):
     _attrs = ('scheme', 'username', 'password', 'hostname',
               'port', 'path', 'params', 'query', 'fragment')
 
-    def __init__(self, url_str):
-        up = parse_url(url_str)
+    def __init__(self, url_str, encoding=None):
+        encoding = encoding or DEFAULT_ENCODING
+        up = parse_url(url_str, encoding=encoding)
+        self.encoding = encoding
         _d = unicode()
         for attr in self._attrs:
             setattr(self, attr, getattr(up, attr) or _d)
@@ -75,15 +87,20 @@ class URL(object):
                 ret.extend([':', self.password])
             ret.append('@')
         ret.append(self.hostname)
-        port = str(self.port)
+        port = unicode(self.port)
         if port:
             ret.extend([':', self.port])
         return u''.join(ret)
+
+    @property
+    def query_string(self):
+        return  # if there are args, join them order
 
     def __iter__(self):
         s, i = self, iter
         return i((s.scheme, s.netloc, s.path, s.params, s.query, s.fragment))
 
-    def __str__(self):
+    def encode(self, encoding=None):
+        encoding = encoding or DEFAULT_ENCODING
         # TODO: inherit or duck type
         return urlunparse(self).encode('utf-8')  # TODO
