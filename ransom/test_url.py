346171c5dd6694d36471ea4765848db252877711
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-05-19 18:13:34 -0700

    add parse_authority py.test fixture

--- ransom/test_url.py
+++ ransom/test_url.py
@@ -6,7 +6,7 @@
 
 import pytest
 
-from url import URL, _URL_RE
+from url import URL, _URL_RE, parse_authority
 
 
 TEST_URLS = [
@@ -17,7 +17,7 @@ TEST_URLS = [
     'ftp://ftp.rfc-editor.org/in-notes/tar/RFCs0001-0500.tar.gz',
     'http://[1080:0:0:0:8:800:200C:417A]/index.html',
     'ssh://192.0.2.16:22/',
-    'https://::101.45.75.219/?hi=bye',
+    'https://[::101.45.75.219]:80/?hi=bye',
     'ldap://[::192.9.5.5]/dc=example,dc=com??sub?(sn=Jensen)',
     'mailto:me@example.com?to=me@example.com&body=hi%20http://wikipedia.org',
     'news:alt.rec.motorcycle',
@@ -32,9 +32,25 @@ def test_url(request):
     return param
 
 
+@pytest.fixture(scope="module", params=TEST_URLS)
+def test_authority(request):
+    match = _URL_RE.match(request.param)
+    return match.groupdict()['authority']
+
+
 def test_regex(test_url):
     match = _URL_RE.match(test_url)
     assert match.groupdict()
+
+
+def test_parse_authorities(test_authority):
+    if not test_authority:
+        return True
+    else:
+        print test_authority
+        family, host, port = parse_authority(test_authority)
+        print family, host, port
+        assert bool(host)
 
 
 def test_basic():
