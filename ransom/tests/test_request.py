edfe9591085aef4dc074224e7a8bdf6df7330ae6
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2014-03-13 02:44:55 -0700

    actually do some very minor request testing (and comment out a pdb)

--- ransom/tests/test_request.py
+++ ransom/tests/test_request.py
@@ -5,5 +5,9 @@ from ransom.request import Request
 
 def test_request_basic():
     req = Request(url='//google.com')
-    print repr(req.to_bytes())
-    import pdb;pdb.set_trace()
+    assert req.host == 'google.com'
+    assert req._url.path == '/'
+    req_str = req.to_bytes()
+    print repr(req_str)
+    assert 'Host:' in req_str
+    #import pdb;pdb.set_trace()
