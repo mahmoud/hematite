ef6ab5c83f5c982d798d4fe71c4f141a9216470c
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2014-03-15 02:28:47 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
+# -*- coding: utf-8 -*-
+
 from setuptools import setup, find_packages
 
 with open('requirements.txt') as f:
     requirements = list(f)
 
-if __name__ == '__main__':
-    setup(name='ransom',
-          version='0.0',
-          install_requires=requirements,
-          packages=find_packages())
+setup(name='hematite',
+      version='0.0.1',
+      author='Mahmoud Hashemi and Mark Williams',
+      url='https://github.com/mahmoud/hematite',
+      install_requires=requirements,
+      packages=find_packages(),
+      license='BSD',
+      classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: HTTP Servers']
+
+      )
