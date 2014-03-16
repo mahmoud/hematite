065bb0ec4f1fd8dac4a9727962d27bfaa05274e8
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2014-03-15 19:11:57 -0700

    add an idealized async example (API not final, just demonstrating an optimistic level of simplicity)

--- client_example.py
+++ client_example.py
@@ -7,3 +7,16 @@ resp = client.get('http://en.wikipedia.org/wiki/Coffee')
 resp_data = resp.get_data()
 
 print resp_data[:1024]
+
+
+def ideal_async():
+    from hematite import async
+    urls = ['http://en.wikipedia.org/wiki/Coffee',
+            'http://en.wikipedia.org/wiki/Tea']
+
+    client = Client()
+    reqs = [client.get(u, async=True) for u in urls]
+    async.join(reqs, timeout=30.0)
+
+    for req in reqs:
+        print req.response
