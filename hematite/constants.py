f5874be9ee7550485b44a0450db3c4b36df1beb8
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2014-03-18 23:29:33 -0700

    add some useful non-RFC2616 response codes

--- hematite/constants.py
+++ hematite/constants.py
@@ -94,6 +94,7 @@ del _init_headers
 
 CODE_REASONS = OMD([(100, 'Continue'),
                     (101, 'Switching Protocols'),
+                    (102, 'Processing'),  # RFC2518
                     (200, 'OK'),
                     (201, 'Created'),
                     (202, 'Accepted'),
@@ -126,11 +127,24 @@ CODE_REASONS = OMD([(100, 'Continue'),
                     (415, 'Unsupported Media Type'),
                     (416, 'Requested range not satisfiable'),
                     (417, 'Expectation Failed'),
+                    (418, "I'm a teapot"),  # RFC2324
+                    (422, 'Unprocessable Entity'),  # RFC4918
+                    (423, 'Locked'),  # RFC4918
+                    (424, 'Failed Dependency'),  # RFC4918
+                    (426, 'Upgrade Required'),  # RFC2817
+                    (428, 'Precondition Required'),  # RFC6585
+                    (429, 'Too Many Requests'),  # RFC6585
+                    (431, 'Request Header Fields Too Large'),  # RFC6585
                     (500, 'Internal Server Error'),
                     (501, 'Not Implemented'),
                     (502, 'Bad Gateway'),
                     (503, 'Service Unavailable'),
                     (504, 'Gateway Time-out'),
-                    (505, 'HTTP Version not supported')])
+                    (505, 'HTTP Version not supported'),
+                    (507, 'Insufficient Storage'),  # RFC4918
+                    (508, 'Loop Detected'),  # RFC5842
+                    (510, 'Not Extended'),  # RFC2774
+                    (511, 'Network Authentication Required')  # RFC6585
+                    ])
 
 REASON_CODES = CODE_REASONS.inverted()
