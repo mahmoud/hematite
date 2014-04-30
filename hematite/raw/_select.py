341b90940b5593a92e365036b0e196e10b5f84a6
Mark Williams <markrwilliams@gmail.com>
2014-04-30 01:24:03 -0700

    working version of non blocking bodies

--- hematite/raw/_select.py
+++ hematite/raw/_select.py
@@ -5,47 +5,96 @@ import select
 
 from hematite.url import URL
 from hematite.socket_io import iopair_from_socket
-from hematite.raw import core, messages as m, envelope as e
+from hematite.raw import core, messages as m, envelope as e, body as b
 
 
 class RequestResponsePair(object):
 
-    def __init__(self, sock, input, output):
+    def __init__(self, sock, input_envelope):
         self.sock = sock
         fn = sock.fileno()
         self.fileno = lambda: fn
         self.reader, self.writer = iopair_from_socket(sock)
-        self.state = m.Empty
-        self.input = input
-        self.output = output
-        self._writer_iter = input._make_writer()
+        self.input_envelope = input_envelope
+        self.output_envelope = e.ResponseEnvelope()
+        self.output_body = None
+        self.body_bits = []
 
-    @classmethod
+        self.body_ready = False
+
+        self._writer_iter = input_envelope._make_writer()
+
+    @property
     def complete(self):
         return self.state is m.Complete
 
-    def write_request(self):
+    def write_request_headers(self):
         if not self.writer.empty:
             self.writer.write(None)
 
         next_bit = next(self._writer_iter, m.Empty)
         if next_bit is m.Empty:
-            self.state = self.output.state
+            self.state = self.output_envelope.state
             return True
         self.writer.write(next_bit.value)
         return False
 
-    def read_response(self):
-        while self.state.type != m.Complete.type:
+    def read_response_headers(self):
+        while not self.complete:
             if self.state.type == m.NeedLine.type:
                 line = core.readline(self.reader)
                 next_state = m.HaveLine(value=line)
-            elif self.state.type != m.Complete.type:
-                assert "Unknown state", self.state
-            self.state = self.output.reader.send(next_state)
-
+            else:
+                raise RuntimeError('Unknown state {0}'.format(self.state))
+            self.state = self.output_envelope.reader.send(next_state)
+        assert self.complete, "Unknown state {0}".format(self.state)
         return self.complete
 
+    def prep_response_body(self):
+        h = self.output_envelope.headers
+        if b.Body(h).chunked:
+            self.output_body = b.ChunkEncodedBody(h)
+            self.read_response_body = self.read_response_body_chunk
+        else:
+            self.output_body = b.IdentityEncodedBody(h)
+        self.state = self.output_body.state
+
+    def read_response_body_chunk(self):
+        data = None
+        while not self.complete:
+            if self.state.type == m.NeedLine.type:
+                line = core.readline(self.reader)
+                next_state = m.HaveLine(value=line)
+            elif self.state.type == m.NeedData.type:
+                data = self.reader.read(self.state.amount)
+                if data is None:
+                    raise io.BlockingIOError(None, None)
+                next_state = m.HaveData(value=data)
+            elif self.state.type == m.NeedPeek.type:
+                peeked = self.reader.peek(self.state.amount)
+                next_state = m.HavePeek(amount=peeked)
+            elif self.state.type == m.HaveData.type:
+                self.body_bits.append(self.state.value)
+                next_state = m.Empty
+            else:
+                raise RuntimeError('Unknown state {0}'.format(self.state))
+            self.state = self.output_body.reader.send(next_state)
+
+        assert self.complete, 'Unknown state {0}'.format(self.state)
+        return self.complete
+
+    def read_response_body(self, amt=None):
+        while not self.complete:
+            if self.state.type == m.NeedData.type:
+                data = self.reader.read(amt or self.state.amount)
+                self.body_bits.append(data)
+                next_state = m.HaveData(value=data)
+            else:
+                raise RuntimeError('Unknown state {0}'.format(self.state))
+            self.state = self.output_body.reader.send(next_state)
+
+        assert self.complete, 'Unknown state {0}'.format(self.state)
+        return self.complete
 
 
 def join(urls):
@@ -57,8 +106,6 @@ def join(urls):
                                    e.Headers([('Host', url.host),
                                               ('User-Agent', 'test'),
                                               ('Accept', '*/*')]))
-        rawresp = e.ResponseEnvelope()
-
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.setblocking(False)
         try:
@@ -67,8 +114,14 @@ def join(urls):
             if exc[0] != errno.EINPROGRESS:
                 raise
 
-        writers.append(RequestResponsePair(sock, rawreq, rawresp))
+        writers.append(RequestResponsePair(sock, rawreq))
+
     finished = []
+
+    def read_body(reader):
+        if reader.read_response_body():
+            readers.remove(reader)
+            finished.append((reader.output_envelope, reader.body_bits))
 
     while readers or writers:
         next_readers = []
@@ -76,20 +129,22 @@ def join(urls):
 
         for writer in write_ready:
             try:
-                if writer.write_request():
+                if writer.write_request_headers():
                     writers.remove(writer)
                     next_readers.append(writer)
             except io.BlockingIOError:
-                print 'blocking write'
                 pass
 
         for reader in read_ready:
             try:
-                if reader.read_response():
-                    readers.remove(reader)
-                    finished.append(reader.output)
+                if reader.body_ready:
+                    read_body(reader)
+                else:
+                    if reader.read_response_headers():
+                        reader.body_ready = True
+                        reader.prep_response_body()
+                        read_body(reader)
             except io.BlockingIOError:
-                print 'blocking read'
                 pass
 
         readers.extend(next_readers)
@@ -115,6 +170,6 @@ if __name__ == '__main__':
 
     try:
         responses = _main()
-        import pdb;pdb.set_trace()
+        import pdb; pdb.set_trace()
     except Exception as e:
         import pdb;pdb.post_mortem()
--- hematite/raw/body.py
+++ hematite/raw/body.py
@@ -13,8 +13,7 @@ class InvalidChunk(BodyReadException):
 
 
 class Body(object):
-    def __init__(self, io_obj, headers):
-        self.io_obj = io_obj
+    def __init__(self, headers):
         norm_headers = dict([(k.lower(), v) for k, v in headers.items()])
         try:
             self.content_length = int(norm_headers['content-length'])
@@ -27,44 +26,53 @@ class Body(object):
             conn_close = False
         self.connection_close = conn_close
 
+        try:
+            c = norm_headers['transfer-encoding'].lower().startswith('chunked')
+        except:
+            c = False
+        self.chunked = c
+
         self.read_amt = 0
         self.closed = False
-        if not (self.content_length or self.connection_close):
+        if not any([self.content_length, self.connection_close, self.chunked]):
             # TODO: check for multipart/byteranges
-            pass
+            raise BodyReadException("Can't read lengthless body")
+        self.state = m.Empty
 
 
 class IdentityEncodedBody(Body):
+    DEFAULT_AMT = 1024
 
     def __init__(self, *args, **kwargs):
         super(IdentityEncodedBody, self).__init__(*args, **kwargs)
-        self._reader = self._make_reader()
-        self.loop = self._make_loop()
-        self.state = next(self._reader)
+        assert not self.chunked
+
+        self.reader = self._make_reader()
+        self.state = next(self.reader)
 
     @property
     def complete(self):
         return self.state.type == m.Complete.type
 
-    def _make_loop(self):
+    def _make_reader(self):
+        to_read = self.content_length or self.DEFAULT_AMT
+
         while not self.complete:
-            if self.state.type == m.NeedData:
-                data = self.io_obj.read(self.state)
+            t, read = yield m.NeedData(amount=to_read)
+            assert t == m.HaveData.type
 
-    def _make_reader(self, size=-1):
-        if self.content_length:
-            size = (self.content_length if size < 0
-                    else min(self.content_length, size))
-        if self.content_length and self.read_amt == self.content_length \
-           or self.closed:
-            return ''
+            if not len(read):
+                self.state = m.Complete
+            else:
+                self.read_amt += len(read)
 
-        ret = self.io_obj.read(size)
-        if not ret:
-            self.closed = True
+                if self.content_length:
+                    to_read = (self.content_length - self.read_amt)
 
-        self.read_amt += len(ret)
-        return ret
+                if to_read <= 0:
+                    self.state = m.Complete
+        while True:
+            yield m.Complete
 
 
 class ChunkEncodedBody(Body):
@@ -72,9 +80,10 @@ class ChunkEncodedBody(Body):
 
     def __init__(self, *args, **kwargs):
         super(ChunkEncodedBody, self).__init__(*args, **kwargs)
-        self._reader = self._make_reader()
-        self.state = next(self._reader)
-        self.loop = self.loop()
+        assert self.chunked
+
+        self.reader = self._make_reader()
+        self.state = next(self.reader)
         self.reset()
         self.ready = False
 
@@ -87,33 +96,6 @@ class ChunkEncodedBody(Body):
         self.read = 0
         self.partials = []
 
-    def _make_loop(self):
-        while not self.complete:
-            if self.state.type == m.NeedLine.type:
-                line = core.readline(self.io_obj)
-                next_state = m.HaveLine(value=line)
-            elif self.state.type == m.NeedData.type:
-                data = self.io_obj.read(self.state.value)
-                next_state = m.HaveData(value=data)
-            elif self.state.type == m.NeedPeek.type:
-                peeked = self.io_obj.peek(self.state.value)
-                next_state = m.HavePeek(value=peeked)
-            elif self.state.type == m.HaveData.type:
-                yield self.state.value
-                self.state = m.NeedLine
-            elif self.state.type == m.Complete.type:
-                pass
-            else:
-                assert "Unknown state", self.state
-            self.state = self._reader.send(next_state)
-
-        return
-
-    def read_chunk(self):
-        if self.complete:
-            return ''
-        return next(self.loop)
-
     def _make_reader(self):
         while not self.complete:
             t, chunk_header = yield m.NeedLine
@@ -125,8 +107,8 @@ class ChunkEncodedBody(Body):
             if not self.IS_HEX.match(chunk_header):
                 raise InvalidChunk('Could not read chunk header', chunk_header)
 
-                # trailing CLRF?
-                self.chunk_length = int(chunk_header, 16)
+            # trailing CLRF?
+            self.chunk_length = int(chunk_header, 16)
 
             if self.chunk_length > core.MAXLINE:
                 raise InvalidChunk('Requested too large a chunk',
@@ -134,13 +116,14 @@ class ChunkEncodedBody(Body):
 
             last = ''
             while self.read < self.chunk_length:
-                t, last = yield m.NeedData(amount=self.chunk_length - len(last))
+                t, last = yield m.NeedData(amount=self.chunk_length
+                                           - self.read)
                 assert t == m.HaveData.type
 
                 if not last:
                     raise core.EndOfStream
 
-                self.read -= len(last)
+                self.read += len(last)
                 self.partials.append(last)
 
             chunk = ''.join(self.partials)
@@ -162,10 +145,11 @@ class ChunkEncodedBody(Body):
             assert t == m.HaveData.type
 
             if not self.chunk_length and not chunk:
-                self.complete = True
-
-            self.reset()
-            yield m.HaveData(chunk)
+                self.state = m.Complete
+            else:
+                self.reset()
+                state = yield m.HaveData(value=chunk)
+                assert state is m.Empty
 
         while True:
             yield m.Complete
