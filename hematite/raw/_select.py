cc8e5aa8083f18a481648dc0fa8290dfa49849de
Mark Williams <markrwilliams@gmail.com>
2014-05-02 09:50:46 -0700

    distinguish between EoS on readline and EAGAIN; writer eviction on connection timeout; prettification

--- hematite/raw/_select.py
+++ hematite/raw/_select.py
@@ -9,9 +9,61 @@ from hematite.socket_io import iopair_from_socket
 from hematite.raw import core, messages as m, envelope as e, body as b
 
 
+def readline(io_obj, sock):
+    try:
+        return core.readline(io_obj)
+    except core.EndOfStream:
+        try:
+            if not sock.recv(1, socket.MSG_PEEK):
+                raise
+        except socket.error as e:
+            if e.errno != errno.EAGAIN:
+                raise
+        raise io.BlockingIOError(None, None)
+
+
+class ConnectionError(Exception):
+    pass
+
+
 class RequestResponsePair(object):
 
-    def __init__(self, sock, input_envelope):
+    @classmethod
+    def connect(cls, urls):
+        if not isinstance(urls, list) or isinstance(urls, tuple):
+            urls = [urls]
+
+        instances = []
+        for url in urls:
+            rawreq = e.RequestEnvelope(e.RequestLine('GET',
+                                                     url.path or '/',
+                                                     e.HTTPVersion(1, 1)),
+                                       e.Headers([('Host', url.host),
+                                                  ('User-Agent', 'test'),
+                                                  ('Accept', '*/*')]))
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.setblocking(0)
+            try:
+                result = sock.connect_ex((url.host, url.port or 80))
+            except socket.error as exc:
+                result = exc.args[0]
+
+            if result:
+                if result not in (errno.EISCONN, errno.EWOULDBLOCK,
+                                  errno.EINPROGRESS, errno.EALREADY):
+                    socket.error('Unknown', result)
+
+            err = sock.getsockopt(socket.SOL_SOCKET, socket.SO_ERROR)
+            if err:
+                raise socket.error('Unknown', err)
+
+            instances.append(cls(url, sock, rawreq))
+
+        return instances
+
+    def __init__(self, url, sock, input_envelope):
+        self.retries = 0
+        self.url = url
         self.sock = sock
         fn = sock.fileno()
         self.fileno = lambda: fn
@@ -20,8 +72,9 @@ class RequestResponsePair(object):
         self.output_envelope = e.ResponseEnvelope()
         self.output_body = None
         self.body_bits = []
-
+        self.state = m.Empty
         self.body_ready = False
+        self.started = time.time()
 
         self._writer_iter = input_envelope._make_writer()
 
@@ -43,7 +96,7 @@ class RequestResponsePair(object):
     def read_response_headers(self):
         while not self.complete:
             if self.state.type == m.NeedLine.type:
-                line = core.readline(self.reader)
+                line = readline(self.reader, self.sock)
                 next_state = m.HaveLine(value=line)
             else:
                 raise RuntimeError('Unknown state {0}'.format(self.state))
@@ -64,7 +117,7 @@ class RequestResponsePair(object):
         data = None
         while not self.complete:
             if self.state.type == m.NeedLine.type:
-                line = core.readline(self.reader)
+                line = readline(self.reader, self.sock)
                 next_state = m.HaveLine(value=line)
             elif self.state.type == m.NeedData.type:
                 data = self.reader.read(self.state.amount)
@@ -73,6 +126,8 @@ class RequestResponsePair(object):
                 next_state = m.HaveData(value=data)
             elif self.state.type == m.NeedPeek.type:
                 peeked = self.reader.peek(self.state.amount)
+                if not peeked:
+                    raise io.BlockingIOError(None, None)
                 next_state = m.HavePeek(amount=peeked)
             elif self.state.type == m.HaveData.type:
                 self.body_bits.append(self.state.value)
@@ -100,35 +155,33 @@ class RequestResponsePair(object):
         return self.complete
 
 
-def join(urls):
-    readers, writers = [], []
-    for url in urls:
-        rawreq = e.RequestEnvelope(e.RequestLine('GET',
-                                                 url.path or '/',
-                                                 e.HTTPVersion(1, 1)),
-                                   e.Headers([('Host', url.host),
-                                              ('User-Agent', 'test'),
-                                              ('Accept', '*/*')]))
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.setblocking(False)
-        try:
-            sock.connect((url.host, url.port or 80))
-        except socket.error as exc:
-            if exc[0] != errno.EINPROGRESS:
-                raise
-
-        writers.append(RequestResponsePair(sock, rawreq))
-
-    finished = []
+def join(urls, timeout=1, retries=1):
+    writers = RequestResponsePair.connect(urls)
+    readers, finished = [], []
 
     def read_body(reader):
         if reader.read_response_body():
             readers.remove(reader)
-            finished.append((reader.output_envelope, reader.body_bits))
+            finished.append((reader.url,
+                             reader.output_envelope,
+                             reader.body_bits))
 
     while readers or writers:
         next_readers = []
-        read_ready, write_ready, _ = select.select(readers, writers, [])
+        read_ready, write_ready, _ = select.select(readers, writers,
+                                                   [], timeout)
+
+        for missed in set(writers) - set(write_ready):
+            # m.Empty means this writer hasn't connected yet
+            if (missed.state is m.Empty
+                and time.time() - missed.started >= timeout):
+                if missed.retries < retries:
+                    missed.retries += 1
+                    missed.started = time.time()
+                else:
+                    # evict this
+                    print 'Connection to {0} timed out'.format(missed.url)
+                    writers.remove(missed)
 
         for writer in write_ready:
             try:
@@ -148,7 +201,6 @@ def join(urls):
                         reader.prep_response_body()
                         read_body(reader)
             except io.BlockingIOError:
-                print 'blocking read', time.time()
                 pass
 
         readers.extend(next_readers)
@@ -169,8 +221,11 @@ if __name__ == '__main__':
 
         if args.shouldprint:
             for j in joined:
-                print j[0].to_bytes(),
-                print ''.join(j[1]),
+                url, headers, body_bits = j
+                print url
+                print headers.to_bytes(),
+                print 'body length', len(''.join(body_bits))
+                print '*' * 78
         return joined
 
     try:
