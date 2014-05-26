6f443c10a7365623273ec5fccfc71d143ba1e584
Mark Williams <markrwilliams@gmail.com>
2014-05-25 23:43:33 -0700

    __main__ clause in setup.py so that a simple py.test invocation does encounter a spurious exception

--- setup.py
+++ setup.py
@@ -5,16 +5,17 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     requirements = list(f)
 
-setup(name='hematite',
-      version='0.0.1',
-      author='Mahmoud Hashemi and Mark Williams',
-      url='https://github.com/mahmoud/hematite',
-      install_requires=requirements,
-      packages=find_packages(),
-      license='BSD',
-      classifiers=[
-        'Intended Audience :: Developers',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: HTTP Servers']
+if __name__ == '__main__':
+    setup(name='hematite',
+          version='0.0.1',
+          author='Mahmoud Hashemi and Mark Williams',
+          url='https://github.com/mahmoud/hematite',
+          install_requires=requirements,
+          packages=find_packages(),
+          license='BSD',
+          classifiers=[
+            'Intended Audience :: Developers',
+            'Topic :: Internet :: WWW/HTTP',
+            'Topic :: Internet :: WWW/HTTP :: HTTP Servers']
 
-      )
+          )
