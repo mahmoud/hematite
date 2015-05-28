4ef6d4c5fc781b4887696326105aff36ccc74969
Mahmoud Hashemi <mahmoud@hatnote.com>
2015-05-27 19:47:02 -0700

    tweaking setup for easier install

--- setup.py
+++ setup.py
@@ -2,20 +2,14 @@
 
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-    requirements = list(f)
-
 if __name__ == '__main__':
     setup(name='hematite',
-          version='0.0.1',
+          version='0.0.2',
           author='Mahmoud Hashemi and Mark Williams',
           url='https://github.com/mahmoud/hematite',
-          install_requires=requirements,
+          install_requires=[],
           packages=find_packages(),
           license='BSD',
-          classifiers=[
-            'Intended Audience :: Developers',
-            'Topic :: Internet :: WWW/HTTP',
-            'Topic :: Internet :: WWW/HTTP :: HTTP Servers']
-
-          )
+          classifiers=['Intended Audience :: Developers',
+                       'Topic :: Internet :: WWW/HTTP',
+                       'Topic :: Internet :: WWW/HTTP :: HTTP Servers'])
